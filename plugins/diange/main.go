b9613f53a4319cee8aac163c9b6cb6e823562899
miracle0609 <amiracle0609@163.com>
2023-05-29 23:40:00 +0800

    update052918

--- plugins/diange/main.go
+++ plugins/diange/main.go
@@ -138,7 +138,7 @@ func init() {
 
 func getSong(keyword string)(*Result, error) {
 	var resp Result
-	api := "http://64.112.43.106:3000/search?keywords=" + url.QueryEscape(keyword) + "&limit=1"
+	api := "http://64.112.43.106:3000/search?keywords=" + url.QueryEscape(keyword)
 	if err := req.C().SetBaseURL(api).Post().Do().Into(&resp); err != nil {
 			fmt.Println(resp)
 			return nil, err
